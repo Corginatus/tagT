35de266052f2120653888864eb9bbafbbe8b21aa
Pingachguk <imayakimov@gmail.com>
2022-01-31 20:14:41 +0300

    UPD

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\UploadFile;
 use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-    //
+    public function upload(Request $request) {
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $uploader = new UploadFile();
+            $path = $uploader->uploadFile($image);
+            return response(['data'=>env('APP_URL').$path->path]);
+        }
+    }
 }
