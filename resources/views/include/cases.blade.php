a011b8d91b8c906ed46cb1989b3b9c57ce52cd51
Andrei <imayakimov@gmail.com>
2022-03-31 17:00:21 +0300

    UPD

--- resources/views/include/cases.blade.php
+++ resources/views/include/cases.blade.php
@@ -1,5 +1,6 @@
 <div class="swiper-container client-carousel-single">
     <div class="swiper-wrapper">
+        @foreach($cases as $case)
         <div class="swiper-slide">
             <div class="col" data-aos="fade-up">
                 <a href="portfolio.html" class="text-reset">
@@ -8,13 +9,14 @@
                             <img src="images/ui/project-01.jpg" alt="" class="img-fluid">
                         </picture>
                         <div class="card-footer bg-white">
-                            <h5 class="font-weight-semibold mb-1">Client</h5>
-                            <p class="font-size-sm mb-0">Category</p>
+                            <h5 class="font-weight-semibold mb-1">{{ $case->client  }}</h5>
+                            <p class="font-size-sm mb-0">{{ $case->type }}</p>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
+        @endforeach
     </div>
     <div class="swiper-pagination text-center position-relative pt-4"></div>
 </div>
