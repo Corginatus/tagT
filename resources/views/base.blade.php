ce9cae67967a69311066307a34bd1b2a3cf23ee8
Andrei <imayakimov@gmail.com>
2022-04-14 20:12:35 +0300

    UPD

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,13 +9,10 @@
         <meta name=description content="Tag">
         <meta name=keywords content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, start-up, parallax, masonry">
         <meta name=author content="uichamp.com">
-        <title>Tag</title>
+        <title>Tag: Leader in digital marketing for Lifestyle, Luxury, Cosmetics, Fashion & Beauty Brands</title>
 
         <!-- Favicons -->
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
-        <link rel="shortcut icon" href="images/favicons/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
         <meta name="msapplication-TileColor" content="#ffffff">
         <meta name="msapplication-TileImage" content="/images/favicons/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
